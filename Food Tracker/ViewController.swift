2c3b4a7873f1bc0a6e722b51ece5e8447d020fcf
Siddharth Rao <sMac@Siddharths-MacBook-Pro.local>
2015-09-21 23:16:28 -0700

    Initial files

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Siddharth Rao on 9/21/15.
+//  Copyright © 2015 Siddharth Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
