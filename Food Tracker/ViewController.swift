6f2e19cb68fd00555cb2077c2e240e24533f226f
Siddharth Rao <sidgrao@gmail.com>
2015-09-22 02:12:06 -0700

    Added a few actions to change the label text

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        mealNameLabel.text = "Default Text"
+    }
 }
 
